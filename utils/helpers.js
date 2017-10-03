7894b6734606e26777637e2e85c163b5c994e051
inkymaze <nickwcannon@gmail.com>
2017-10-03 10:08:27 -0700

    add entry actions and reset info

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View } from 'react-native'
-import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { white } from './colors'
 
 export function getMetricMetaInfo (metric) {
@@ -142,62 +142,4 @@ export function timeToString (time = Date.now()) {
   const date = new Date(time)
   const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
   return todayUTC.toISOString().split('T')[0]
-}
-
-// utils/_calendar.js
-
-import { AsyncStorage } from 'react-native'
-import { getMetricMetaInfo, timeToString } from './helpers'
-
-export const CALENDAR_STORAGE_KEY = 'UdaciFitness:calendar'
-
-function getRandomNumber (max) {
- return Math.floor(Math.random() * max) + 0
-}
-
-function setDummyData () {
- const { run, bike, swim, sleep, eat } = getMetricMetaInfo()
-
- let dummyData = {}
- const timestamp = Date.now()
-
- for (let i = -183; i < 0; i++) {
-   const time = timestamp + i * 24 * 60 * 60 * 1000
-   const strTime = timeToString(time)
-   dummyData[strTime] = getRandomNumber(3) % 2 === 0
-     ? {
-         run: getRandomNumber(run.max),
-         bike: getRandomNumber(bike.max),
-         swim: getRandomNumber(swim.max),
-         sleep: getRandomNumber(sleep.max),
-         eat: getRandomNumber(eat.max),
-       }
-     : null
- }
-
- AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(dummyData))
-
- return dummyData
-}
-
-function setMissingDates (dates) {
- const length = Object.keys(dates).length
- const timestamp = Date.now()
-
- for (let i = -183; i < 0; i++) {
-   const time = timestamp + i * 24 * 60 * 60 * 1000
-   const strTime = timeToString(time)
-
-   if (typeof dates[strTime] === 'undefined') {
-     dates[strTime] = null
-   }
- }
-
- return dates
-}
-
-export function formatCalendarResults (results) {
- return results === null
-   ? setDummyData()
-   : setMissingDates(JSON.parse(results))
 }
